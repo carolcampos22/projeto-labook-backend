3b5756c2b30747625298df777f34335ef01cd80f
carolcampos22 <mendes.carolina2209@gmail.com>
2023-08-20 13:53:03 -0300

    Criação dos DTOs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import { userRouter } from './router/userRouter'
+import { postRouter } from './router/postRouter'
 
 dotenv.config()
 
@@ -17,3 +19,5 @@ app.get("/ping", ((req, res) => {
     res.send("Pong!!!!")
 }))
 
+app.use("/users", userRouter)
+app.use("/posts", postRouter)
