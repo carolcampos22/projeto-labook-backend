5111c0eaf32b6f971ef1ac5d5965cc0ced260f87
carolcampos22 <mendes.carolina2209@gmail.com>
2023-08-19 21:40:17 -0300

    Configurações da API, modelagem do banco de dados, refatoração para implementação de POO, correções

--- src/database/labook.sql
+++ src/database/labook.sql
@@ -5,44 +5,61 @@ CREATE TABLE users(
     email TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
     role TEXT NOT NULL,
-    created_at TEXT NOT NULL DEFAULT(DATETIME())
+    created_at TEXT DEFAULT(DATETIME()) NOT NULL 
 );
 
 CREATE TABLE posts(
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
-    creator_id TEXT NOT NULL,
+    creator_id TEXT UNIQUE NOT NULL,
     content TEXT NOT NULL,
-    likes INTEGER NOT NULL,
-    dislikes INTEGER NOT NULL,
-    created_at TEXT NOT NULL DEFAULT(DATETIME()),
-    updated_at TEXT NOT NULL DEFAULT(DATETIME()),
+    likes INTEGER DEFAULT (0) NOT NULL,
+    dislikes INTEGER DEFAULT (0) NOT NULL,
+    created_at TEXT DEFAULT(DATETIME()) NOT NULL,
+    updated_at TEXT DEFAULT(DATETIME()) NOT NULL,
     FOREIGN KEY (creator_id) REFERENCES users(id)
-
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
 
 CREATE TABLE likes_dislikes(
     user_id TEXT NOT NULL,
     post_id TEXT NOT NULL,
-    like INTEGER,
-    FOREIGN KEY (user_id) REFERENCES users(id),
+    like INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
     FOREIGN KEY (post_id) REFERENCES posts(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
 
 INSERT INTO users(id, name, email, password, role)
 VALUES
-    ("u001","Luiz Silva", "lsilva@gmail.com", "123abc", "NORMAL"),
-    ("u002","Joana Maciel", "maciel_jo@gmail.com", "123456", "NORMAL"),
-    ("u003","Caio Cézar", "cezar9840958@hotmail.com", "47389oiwo", "NORMAL");
+    ("u001","Luiz Silva", "lsilva@gmail.com", "$2a$12$ukmUVHppNZ2vGlt//iD4uOgTrUNCdy8QWAfw0LluJCkoJRvIRW/rK
+", "NORMAL"), --senha: 123abc
+    ("u002","Joana Maciel", "maciel_jo@gmail.com", "$2a$12$kAW9Gux58tiTFlvn4vu1AOPs3FxWJB0vBFNMUH.BYUyNQBTd62U.C
+", "NORMAL"), --senha 123456
+    ("u003","Caio Cézar", "cezar9840958@hotmail.com", "$2a$12$jNIp3gROjKJuUB1bVd7kbO96KZK83JdeB.cDJjRPU4RCXz6NG9ife
+", "NORMAL");--senha 47389oiwo
 
-INSERT INTO posts(id, creator_id, content, likes, dislikes)
+INSERT INTO posts(id, creator_id, content)
 VALUES
-    ("p001", "u001", "Partiu Região dos Lagos!", 15, 0),
-    ("p002", "u003", "Festa do Luiz nesse fds, quem vai?", 80, 2);
+    ("p001", "u001", "Partiu Região dos Lagos!"),
+    ("p002", "u003", "Festa do Luiz nesse fds, quem vai?");
 
 INSERT INTO likes_dislikes(user_id, post_id, like)
 VALUES
-    ("u001", "p001", 1),
-    ("u001", "p002", 0),
-    ("u002", "p001", 0);
+    ("u002", "p001", 1),
+    ("u003", "p001", 1),
+    ("u001", "p002", 1),
+    ("u003", "p002", 0);
 
 SELECT * FROM likes_dislikes;
+
+UPDATE posts
+SET likes = 2
+WHERE id = 'p001';
+
+UPDATE posts
+SET likes = 1, dislikes = 1
+WHERE id = 'p002';
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,19 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.listen(3003, () => {
-  console.log(`Servidor rodando na porta ${3003}`)
+app.listen(Number(process.env.PORT) || 3003, () => {
+    console.log(`Servidor rodando na porta ${Number(process.env.PORT) || 3003}`)
 })
 
-app.get("/ping", async (req: Request, res: Response) => {
-    try {
-        res.status(200).send({ message: "Pong!" })
-    } catch (error) {
-        console.log(error)
+app.get("/ping", ((req, res) => {
+    res.send("Pong!!!!")
+}))
 
-        if (req.statusCode === 200) {
-            res.status(500)
-        }
-
-        if (error instanceof Error) {
-            res.send(error.message)
-        } else {
-            res.send("Erro inesperado")
-        }
-    }
-})
-
-//criação das tabelas
